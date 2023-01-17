dc10eab03991c2c2e87759bbb58595b0ea43b980
chanhihi <chanhihi55@gmail.com>
2023-01-18 02:49:01 +0900

    feat: pipex init

---
+++ ft_mandatory/pipex.h
@@ -0,0 +1,11 @@
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../lib/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+
+#endif
---
+++ ft_mandatory/pipex_main.c
@@ -0,0 +1,9 @@
+
+#include "pipex.h"
+
+int main(int argc, char **argv)
+{
+	if (argc != 5)
+		perror("It must take 4 arguments")
+	return (0);
+}
