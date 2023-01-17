c3fcf8fe3d0b25dca80d7fa35f47d08e6fcb2ced
chanhihi <chanhihi55@gmail.com>
2023-01-18 02:47:31 +0900

    feat: bonus init

---
+++ ft_bonus/pipex_bonus.h
@@ -0,0 +1,12 @@
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+
+# include "../lib/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+
+
+#endif
---
+++ ft_bonus/pipex_main_bonus.c
@@ -0,0 +1,7 @@
+
+#include "pipex.h"
+
+int main(int argc, char **argv)
+{
+	return (0);
+}
