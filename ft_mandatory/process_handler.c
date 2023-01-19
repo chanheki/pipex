65f991f918d50028263d5505ba33e228e9bee5bf
chanhihi <chanhihi55@gmail.com>
2023-01-19 19:25:26 +0900

    feat: process handler

---
+++ ft_mandatory/process_handler.c
@@ -0,0 +1,36 @@
+
+#include "../includes/pipex.h"
+
+static void	process_child(char **argv, char **envp, int *fd)
+{
+	int	filein;
+
+	filein = open(argv[1], O_RDONLY, 0777);
+	if (filein < 0)
+		error_exitor("filein error");
+	dup2(fd[1], STDOUT_FILENO);
+	dup2(filein, STDIN_FILENO);
+	close(fd[0]);
+	execute(argv[2], envp);
+}
+
+static void	process_parent(char **argv, char **envp, int *fd)
+{
+	int	fileout;
+
+	fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fileout < 0)
+		error_exitor("fileout error");
+	dup2(fd[0], STDIN_FILENO);
+	dup2(fileout, STDOUT_FILENO);
+	close(fd[1]);
+	execute(argv[3], envp);
+}
+
+void	process_handler(char **argv, char **envp, int *fd, t_status status)
+{
+	if (status == CHILD_PROCESS)
+		process_child(argv, envp, fd);
+	if (status == PARENT_PROCESS)
+		process_parent(argv, envp, fd);
+}
