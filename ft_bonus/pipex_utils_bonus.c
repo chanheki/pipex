e6ffad942a380cc930fb42e9fbee53486835df23
chanhihi <chanhihi55@gmail.com>
2023-01-25 19:54:17 +0900

    feat: append pipex init, validator wip: file_checker

--- ft_bonus/pipex_utils_bonus.c
+++ ft_bonus/pipex_utils_bonus.c
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_utils_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 18:54:18 by chanheki          #+#    #+#             */
+/*   Updated: 2023/01/25 19:54:17 by chanheki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-void pipex_init(t_info *info, int argc, char **argv, char **envp)
+void	pipex_init(t_info *info, int argc, char **argv, char **envp)
 {
 	info->argc = argc;
 	info->argv = argv;
@@ -9,19 +20,22 @@ void pipex_init(t_info *info, int argc, char **argv, char **envp)
 	info->fd[0] = -1;
 	info->fd[1] = -1;
 	info->pid = -1;
+	info->infile = -1;
+	info->outfile = -1;
 }
 
-void pipex_validator(t_info *info)
+t_input	pipex_validator(t_info *info)
 {
 	if (!ft_strncmp(info->argv[1], "here_doc", 8))
 	{
 		if (info->argc < 6)
 		{
 			ft_putstr_fd("\033[31mError: here_doc arguments error\n\033[0m", 2);
-			ft_putstr_fd("    ./pipex \"here_doc\" <LIMITER> <cmd> <cmd1> <...> <file>\n", 1);
+			ft_putstr_fd("    ./pipex \"here_doc\" ", 1);
+			ft_putstr_fd("<LIMITER> <cmd> <cmd1> <...> <file>\n", 1);
 			exit(EXIT_FAILURE);
 		}
-		here_doc(info->argv[2], info);
+		return (HERE_DOC);
 	}
 	if (info->argc < 5)
 	{
@@ -29,11 +43,12 @@ void pipex_validator(t_info *info)
 		ft_putstr_fd("Ex: ./pipex <file1> <cmd1> <cmd2> <...> <file2>\n", 1);
 		exit(EXIT_FAILURE);
 	}
+	return (INFILE);
 }
 
 void	here_doc(const char *limiter, t_info *info)
 {
-	char *line;
+	char	*line;
 
 	if (pipe(info->fd) == -1)
 		error_exitor("here_doc pipe error");
@@ -41,12 +56,15 @@ void	here_doc(const char *limiter, t_info *info)
 	if (info->pid == 0)
 	{
 		close(info->fd[0]);
-		while (get_next_line(line))
+		line = get_next_line(0);
+		while (line)
 		{
 			if (!ft_strncmp(line, limiter, ft_strlen(limiter)))
 				exit(EXIT_SUCCESS);
 			write(info->fd[1], line, ft_strlen(line));
+			line = get_next_line(0);
 		}
+		free(line);
 	}
 	else
 	{
@@ -54,4 +72,20 @@ void	here_doc(const char *limiter, t_info *info)
 		dup2(info->fd[0], STDIN_FILENO);
 		wait(NULL);
 	}
+}
+
+int	file_cheker(char *argv, t_input i)
+{
+	int	file;
+
+	file = 0;
+	if (i == HERE_DOC)
+		file = open(argv, O_WRONLY | O_CREAT | O_APPEND, 0777);
+	else if (i == 1)
+		file = open(argv, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	else if (i == 2)
+		file = open(argv, O_WRONLY, 0777);
+	if (file == -1)
+		error_exitor("file check error");
+	return (file);
 }
