1131ee95bc76212bd2b6cb29978cb1c5191d37bd
chanhihi <chanhihi55@gmail.com>
2023-01-30 20:18:41 +0900

    feat: pipex infile outfile

--- ft_bonus/pipex_main_bonus.c
+++ ft_bonus/pipex_main_bonus.c
@@ -15,12 +15,25 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_info	info;
+	int		i;
 
 	pipex_init(&info, argc, argv, envp);
 	if (pipex_validator(&info) == HERE_DOC)
 	{
-		info.infile = file_checker(info.argv, HERE_DOC);
+		i = 3;
+		info.infile = file_checker(info.argv[argc - 1], HERE_DOC);
 		here_doc(info.argv[2], &info);
 	}
+	else
+	{
+		i = 2;
+		info.outfile = file_checker(info.argv[argc - 1], OUTFILE);
+		info.infile = file_checker(info.argv[1], INFILE);
+		dup2(info.infile, STDIN_FILENO);
+	}
+	while (i < argc - 2)
+		child_process(argv[i++], envp);
+	dup2(info.outfile, STDOUT_FILENO);
+	execute(argv[argc - 2], envp);
 	return (0);
 }
