4415fea7bc7465c4673bae668f22983e12828eb2
chanhihi <chanhihi55@gmail.com>
2023-01-31 21:43:40 +0900

    bug: vali argc

--- ft_bonus/pipex_utils_bonus.c
+++ ft_bonus/pipex_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 18:54:18 by chanheki          #+#    #+#             */
-/*   Updated: 2023/01/25 19:54:17 by chanheki         ###   ########.fr       */
+/*   Updated: 2023/01/31 21:43:15 by chanheki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,22 @@ void	pipex_init(t_info *info, int argc, char **argv, char **envp)
 	info->argc = argc;
 	info->argv = argv;
 	info->envp = envp;
+	info->pid = -1;
 	info->fd[0] = -1;
 	info->fd[1] = -1;
-	info->pid = -1;
 	info->infile = -1;
 	info->outfile = -1;
+	info->argv_index = 2;
 }
 
 t_input	pipex_validator(t_info *info)
 {
+	if (info->argc < 5)
+	{
+		ft_putstr_fd("\033[31mError: Bad arguments\n\033[0m", 2);
+		ft_putstr_fd("Ex: ./pipex <file1> <cmd1> <cmd2> <...> <file2>\n", 1);
+		exit(EXIT_FAILURE);
+	}
 	if (!ft_strncmp(info->argv[1], "here_doc", 8))
 	{
 		if (info->argc < 6)
@@ -37,12 +44,6 @@ t_input	pipex_validator(t_info *info)
 		}
 		return (HERE_DOC);
 	}
-	if (info->argc < 5)
-	{
-		ft_putstr_fd("\033[31mError: Bad arguments\n\033[0m", 2);
-		ft_putstr_fd("Ex: ./pipex <file1> <cmd1> <cmd2> <...> <file2>\n", 1);
-		exit(EXIT_FAILURE);
-	}
 	return (INFILE);
 }
 
@@ -50,6 +51,7 @@ void	here_doc(const char *limiter, t_info *info)
 {
 	char	*line;
 
+	info->argv_index = 3;
 	if (pipe(info->fd) == -1)
 		error_exitor("here_doc pipe error");
 	info->pid = fork();
