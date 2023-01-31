8411636468efd805fe26e4dbb4691571ae80f20a
chanhihi <chanhihi55@gmail.com>
2023-01-31 21:44:05 +0900

    feat: bonus main

--- ft_bonus/pipex_main_bonus.c
+++ ft_bonus/pipex_main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:56:04 by chanheki          #+#    #+#             */
-/*   Updated: 2023/01/25 19:52:27 by chanheki         ###   ########.fr       */
+/*   Updated: 2023/01/31 21:37:56 by chanheki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,24 @@
 
 int	main(int argc, char **argv, char **envp)
 {
+	t_input	input_state;
 	t_info	info;
-	int		i;
 
 	pipex_init(&info, argc, argv, envp);
-	if (pipex_validator(&info) == HERE_DOC)
+	input_state = pipex_validator(&info);
+	if (input_state == HERE_DOC)
 	{
-		i = 3;
-		info.infile = file_checker(info.argv[argc - 1], HERE_DOC);
+		info.outfile = file_checker(info.argv[argc - 1], HERE_DOC);
 		here_doc(info.argv[2], &info);
 	}
 	else
 	{
-		i = 2;
-		info.outfile = file_checker(info.argv[argc - 1], OUTFILE);
 		info.infile = file_checker(info.argv[1], INFILE);
+		info.outfile = file_checker(info.argv[argc - 1], OUTFILE);
 		dup2(info.infile, STDIN_FILENO);
 	}
 	while (i < argc - 2)
-		child_process(argv[i++], envp);
+		execute_process(argv[i++], envp);
 	dup2(info.outfile, STDOUT_FILENO);
 	execute(argv[argc - 2], envp);
 	return (0);
