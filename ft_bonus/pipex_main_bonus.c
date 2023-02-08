4d57ef7b98a4ae68f5cc906824bcaf2ee16cd0d9
chanhihi <chanhihi55@gmail.com>
2023-02-08 11:23:05 +0900

    feat: Treat the last process as a child process

--- ft_bonus/pipex_main_bonus.c
+++ ft_bonus/pipex_main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:56:04 by chanheki          #+#    #+#             */
-/*   Updated: 2023/02/01 20:49:16 by chanheki         ###   ########.fr       */
+/*   Updated: 2023/02/07 20:26:37 by chanheki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,23 @@ int	main(int argc, char **argv, char **envp)
 	t_info	info;
 
 	pipex_init(&info, argc, argv, envp);
-	if (pipex_validator(&info) == HERE_DOC)
+	info.status = pipex_validator(&info);
+	if (info.status == HERE_DOC)
 	{
+		here_doc(&info);
+		info.infile = file_checker(info.argv[1], INFILE);
 		info.outfile = file_checker(info.argv[argc - 1], HERE_DOC);
-		here_doc(info.argv[2], &info);
 	}
-	else
+	else if (info.status == INFILE)
 	{
 		info.infile = file_checker(info.argv[1], INFILE);
 		info.outfile = file_checker(info.argv[argc - 1], OUTFILE);
 		dup2(info.infile, STDIN_FILENO);
 	}
+	else
+		error_exitor("status error");
 	while (info.argv_index < argc - 2)
-		execute_process(&info, argv[info.argv_index++], envp);
-	dup2(info.outfile, STDOUT_FILENO);
-	execute(argv[argc - 2], envp);
+		execute_process(&info, info.argv[info.argv_index++], envp);
+	last_process(&info, info.argv[info.argc - 2], envp);
 	return (0);
 }
