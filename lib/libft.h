6f2ea5a30cdfc55e27b05ea109789ba431041820
chanhihi <chanhihi55@gmail.com>
2023-02-08 11:27:52 +0900

    feat: Add configuration files in libft.

--- lib/libft.h
+++ lib/libft.h
@@ -6,7 +6,7 @@
 /*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:40:50 by chanheki          #+#    #+#             */
-/*   Updated: 2022/12/21 23:07:43 by chanheki         ###   ########.fr       */
+/*   Updated: 2023/02/07 19:51:30 by chanheki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,7 @@ size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t		ft_strlen(const char *s);
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int			ft_strcmp(const char *s1, const char *s2);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*ft_strndup(const char *src, int start, int num);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
