55dbaa1c7112011cea8938137822d5f500c66303
chanhihi <chanhihi55@gmail.com>
2023-02-08 11:26:12 +0900

    refact: Error handling refinement

--- ft_mandatory/process_handler.c
+++ ft_mandatory/process_handler.c
@@ -6,7 +6,7 @@
 /*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 22:25:51 by chanheki          #+#    #+#             */
-/*   Updated: 2023/01/31 17:12:17 by chanheki         ###   ########.fr       */
+/*   Updated: 2023/02/07 15:41:39 by chanheki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@ static void	process_child(char **argv, char **envp, int *fd)
 {
 	int	infile;
 
-	infile = open(argv[1], O_RDONLY, 0777);
+	infile = open(argv[1], O_RDONLY);
 	if (infile < 0)
 		error_exitor("infile error");
-	dup2(infile, STDIN_FILENO);
-	dup2(fd[1], STDOUT_FILENO);
-	close(fd[0]);
+	if (dup2(infile, STDIN_FILENO) < 0)
+		error_exitor("dup stdin error");
+	if (dup2(fd[FD_WRITE], STDOUT_FILENO) < 0)
+		error_exitor("dup stdout error");
+	if (close(fd[FD_READ]) < 0)
+		error_exitor("close error");
 	execute(argv[2], envp);
 }
 
@@ -29,12 +32,15 @@ static void	process_parent(char **argv, char **envp, int *fd)
 {
 	int	outfile;
 
-	outfile = open(argv[4], O_RDWR | O_CREAT | O_TRUNC, 0777);
+	outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (outfile < 0)
 		error_exitor("outfile error");
-	dup2(fd[0], STDIN_FILENO);
-	dup2(outfile, STDOUT_FILENO);
-	close(fd[1]);
+	if (dup2(fd[FD_READ], STDIN_FILENO) < 0)
+		error_exitor("dup stdin fd error");
+	if (dup2(outfile, STDOUT_FILENO) < 0)
+		error_exitor("dup stdout fd error");
+	if (close(fd[FD_WRITE]) < 0)
+		error_exitor("close error");
 	execute(argv[3], envp);
 }
 
