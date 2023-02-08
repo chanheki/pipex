8ea42130f510b4e2bade948c862b052e6c72708d
chanhihi <chanhihi55@gmail.com>
2023-02-08 11:24:29 +0900

    feat: constantization & process here doc file

--- ft_bonus/pipex_utils_bonus.c
+++ ft_bonus/pipex_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 18:54:18 by chanheki          #+#    #+#             */
-/*   Updated: 2023/02/01 20:18:50 by chanheki         ###   ########.fr       */
+/*   Updated: 2023/02/07 20:32:49 by chanheki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@ void	pipex_init(t_info *info, int argc, char **argv, char **envp)
 	info->argv = argv;
 	info->envp = envp;
 	info->pid = -1;
-	info->fd[0] = -1;
-	info->fd[1] = -1;
+	info->fd[FD_READ] = -1;
+	info->fd[FD_WRITE] = -1;
 	info->infile = -1;
 	info->outfile = -1;
 	info->argv_index = 2;
+	info->status = ERROR;
 }
 
 t_input	pipex_validator(t_info *info)
@@ -47,33 +48,62 @@ t_input	pipex_validator(t_info *info)
 	return (INFILE);
 }
 
-void	here_doc(const char *limiter, t_info *info)
+// static void	here_doc_compare(const char *line, const char *limiter)
+// {
+// 	if (!ft_strncmp(line, limiter, ft_strlen(limiter))
+// 		&& ft_strlen(line) == ft_strlen(limiter) + 1)
+// 		exit(EXIT_SUCCESS);
+// }
+
+// void	here_doc(const char *limiter, t_info *info)
+// {
+// 	char	*line;
+
+// 	info->argv_index = 3;
+// 	if (pipe(info->fd) == -1)
+// 		error_exitor("here_doc pipe error");
+// 	info->pid = fork();
+// 	if (info->pid == CHILD_PROCESS)
+// 	{
+// 		close(info->fd[FD_READ]);
+// 		line = get_next_line(STDIN_FILENO);
+// 		while (line)
+// 		{
+// 			here_doc_compare(line, limiter);
+// 			write(info->fd[FD_WRITE], line, ft_strlen(line));
+// 			line = get_next_line(STDIN_FILENO);
+// 		}
+// 		free(line);
+// 	}
+// 	else
+// 	{
+// 		dup2(info->fd[FD_READ], STDIN_FILENO);
+// 		close(info->fd[FD_WRITE]);
+// 		close(info->fd[FD_READ]);
+// 		waitpid(info->pid, NULL, WNOWAIT);
+// 	}
+// }
+
+void	here_doc(t_info *info)
 {
 	char	*line;
+	char	*limiter;
+	int		here_doc_fd;
 
 	info->argv_index = 3;
-	if (pipe(info->fd) == -1)
-		error_exitor("here_doc pipe error");
-	info->pid = fork();
-	if (info->pid == 0)
+	here_doc_fd = file_checker(info->argv[1], OUTFILE);
+	line = get_next_line(STDIN_FILENO);
+	limiter = ft_strjoin(info->argv[2], "\n");
+	while (ft_strcmp(line, limiter))
 	{
-		close(info->fd[0]);
-		line = get_next_line(0);
-		while (line)
-		{
-			if (!ft_strncmp(line, limiter, ft_strlen(limiter)))
-				exit(EXIT_SUCCESS);
-			write(info->fd[1], line, ft_strlen(line));
-			line = get_next_line(0);
-		}
 		free(line);
+		ft_putstr_fd(line, here_doc_fd);
+		line = get_next_line(STDIN_FILENO);
 	}
-	else
-	{
-		close(info->fd[1]);
-		dup2(info->fd[0], STDIN_FILENO);
-		waitpid(info->pid, NULL, WNOHANG);
-	}
+	free(limiter);
+	free(line);
+	if (close(here_doc_fd) < 0)
+		error_exitor("here_doc close error");
 }
 
 int	file_checker(char *argv, t_input i)
@@ -82,11 +112,11 @@ int	file_checker(char *argv, t_input i)
 
 	file = 0;
 	if (i == HERE_DOC)
-		file = open(argv, O_WRONLY | O_CREAT | O_APPEND, 0777);
+		file = open(argv, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	else if (i == OUTFILE)
-		file = open(argv, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		file = open(argv, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	else if (i == INFILE)
-		file = open(argv, O_WRONLY, 0777);
+		file = open(argv, O_RDONLY);
 	if (file == -1)
 		error_exitor("file check error");
 	return (file);
