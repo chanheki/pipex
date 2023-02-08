2eba467580cbbc012ab4eacb669fb190540ee0bf
chanhihi <chanhihi55@gmail.com>
2023-02-08 20:55:36 +0900

    feat: info pid

--- ft_bonus/process_handler_bonus.c
+++ ft_bonus/process_handler_bonus.c
@@ -14,14 +14,12 @@
 
 void	execute_process(t_info *info, char *argv, char **envp)
 {
-	pid_t	pid;
-
 	if (pipe(info->fd) == -1)
 		error_exitor("pipe error");
-	pid = fork();
-	if (pid == -1)
-		error_exitor("pid error");
-	if (pid == 0)
+	info->pid = fork();
+	if (info->pid == -1)
+		error_exitor("info->pid error");
+	if (info->pid == 0)
 	{
 		if (dup2(info->fd[FD_WRITE], STDOUT_FILENO) < 0)
 			error_exitor("dup2 fd write error");
@@ -31,40 +29,38 @@ void	execute_process(t_info *info, char *argv, char **envp)
 	}
 	else
 	{
+		waitpid(info->pid, NULL, WNOHANG);
 		if (dup2(info->fd[FD_READ], STDIN_FILENO) < 0)
 			error_exitor("dup2 fd read error");
 		if (close(info->fd[FD_WRITE]) < 0)
 			error_exitor("close fd write error");
 		if (close(info->fd[FD_READ]) < 0)
 			error_exitor("close fd read error");
-		waitpid(pid, NULL, WNOWAIT);
 	}
 }
 
-void	last_process(t_info *info, char *argv, char **envp)
+void	last_process(t_info *info, int index, char **envp)
 {
-	pid_t	pid;
-
 	if (pipe(info->fd) == -1)
 		error_exitor("pipe error");
-	pid = fork();
-	if (pid == -1)
-		error_exitor("pid error");
-	if (pid == 0)
+	info->pid = fork();
+	if (info->pid == -1)
+		error_exitor("info->pid error");
+	if (info->pid == 0)
 	{
-		if (close(info->fd[FD_READ]) < 0)
-			error_exitor("close fd read error");
 		if (dup2(info->outfile, STDOUT_FILENO) < 0)
 			error_exitor("dup2 fd outfile error");
-		execute(argv, envp);
+		if (close(info->fd[FD_READ]) < 0)
+			error_exitor("close fd read error");
+		execute(info->argv[index], envp);
 	}
 	else
 	{
+		waitpid(info->pid, NULL, WNOHANG);
 		if (close(info->fd[FD_WRITE]) < 0)
 			error_exitor("close fd write error");
 		if (close(info->fd[FD_READ]) < 0)
 			error_exitor("close fd read error");
-		waitpid(pid, NULL, WNOWAIT);
 	}
 	if (info->status == HERE_DOC)
 		unlink(info->argv[1]);
